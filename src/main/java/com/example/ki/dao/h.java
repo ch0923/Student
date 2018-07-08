86aeaa95156fed3e4a477f9941c9406bdbefe5c3
ch0923 <wang918222@sina.com>
2018-07-08 10:18:23 +0800

    用户名

--- src/main/java/com/example/ki/dao/h.java
+++ src/main/java/com/example/ki/dao/h.java
@@ -1,4 +1,33 @@
 package com.example.ki.dao;
 
+import java.sql.Date;
+
 public class h {
+    private int id;
+    private String name;
+    private Date time;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
 }
