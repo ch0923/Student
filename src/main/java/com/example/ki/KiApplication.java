2167ce93c31c3500b0a52947acd21cbde471fdb5
ch0923 <wang918222@sina.com>
2018-07-08 09:45:09 +0800

    student

---
+++ src/main/java/com/example/ki/KiApplication.java
@@ -0,0 +1,12 @@
+package com.example.ki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiApplication.class, args);
+	}
+}
